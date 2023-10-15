a5b591ff466d40a63b0b2f34b167e0fc777b6c64
ManuelSpinola <mspinola10@gmail.com>
2023-10-14 21:12:05 -0600

    2nd committ

---
+++ R/cr_hex_grids.R
@@ -0,0 +1,23 @@
+#' @name cr_hex_grid_res_5
+#'
+#' @aliases
+#' cr_hex_grid_res_6
+#' cr_hex_grid_res_7
+#' cr_hex_grid_res_8
+#' cr_hex_grid_res_9
+#'
+#' @title Hierarchical hexagonal geospatial grids for Costa Rica of resolution 5, 6, 7, 8 and 9 (H3, Hexagonal hierarchical geospatial indexing system)
+#'
+#' @description
+#' Hierarchical hexagonal geospatial grids for Costa Rica of resolution 5, 6, 7, 8, and 9 (H3, Hexagonal hierarchical geospatial indexing system).
+#'
+#' @usage
+#' cr_hex_grid_res_5
+#' cr_hex_grid_res_6
+#' cr_hex_grid_res_7
+#' cr_hex_grid_res_8
+#' cr_hex_grid_res_9
+#'
+#' @format An sf object (EPSG:4326)
+#'
+"cr_hex_grid_res_5"
